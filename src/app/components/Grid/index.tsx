c2eb20cd0772c7182a3c1273bd5691ba7211d4f6
Anton Strandqvist <anton.sjolander@hyperisland.se>
2024-03-11 10:55:59 +0100

    added suspense

--- src/app/components/Grid/index.tsx
+++ src/app/components/Grid/index.tsx
@@ -1,5 +1,6 @@
 import { Card } from "./Card";
 import type { ResturantType } from "../../types";
+import { Suspense } from "react";
 export async function Grid({ restaurants }: { restaurants: ResturantType[] }) {
 	return (
 		<div className="w-full px-6 md:pr-4 md:pl-0 2xl:pr-0 pb-6 md:pb-0">
@@ -10,17 +11,20 @@ export async function Grid({ restaurants }: { restaurants: ResturantType[] }) {
 				className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 grid-rows-3"
 				// className="flex flex-wrap gap-4 min-h-[calc(202px*3+1rem)]"
 			>
-				{restaurants.length ? (
-					restaurants.map((item: any) => (
-						<Card {...item} key={item.id} />
-					))
-				) : (
-					<div className="col-span-3">
-						<h2 className="text-h1">
-							Cant find any resturants with the selected filters
-						</h2>
-					</div>
-				)}
+				<Suspense fallback={<div>Loading...</div>}>
+					{restaurants.length ? (
+						restaurants.map((item: any) => (
+							<Card {...item} key={item.id} />
+						))
+					) : (
+						<div className="col-span-3">
+							<h2 className="text-h1">
+								Cant find any resturants with the selected
+								filters
+							</h2>
+						</div>
+					)}
+				</Suspense>
 			</div>
 		</div>
 	);
